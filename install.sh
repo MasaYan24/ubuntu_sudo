9d419021810e9650cc4897f1b6a5d46baf46e44d
takahashi <7567050+MasaYan24@users.noreply.github.com>
2023-04-22 06:16:57 +0900

    Update install.sh
    Only environmental items.

--- install.sh
+++ install.sh
@@ -4,39 +4,7 @@
 sudo apt install -y zsh unzip ruby openssh-server curl cifs-utils
 sudo apt install -y gcc make pkg-config libglvnd-dev  # for NVIDIA driver
 sudo apt install -y tmux
-sudo chsh -s /bin/zsh $(whoami)
-
-# install homebrew
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Neovim install and setting
 sudo apt install -y neovim
-git clone https://github.com/MasaYan24/.vim.git $HOME/
-sh $HOME/.vim/install.sh
+sudo chsh -s /bin/zsh $(whoami)  # this is optional
 
-# Prompto setting
-# sudo sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
-brew install starship
-mkdir -p $HOME/.config && echo "command_timeout = 2000" > $HOME/.config/starship.toml
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-wget https://raw.githubusercontent.com/MasaYan24/zshrc/main/.zshrc -P $HOME/
-
-# Developing tool
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-  -O /tmp/miniconda.sh \
-  && sh /tmp/miniconda.sh -b -p $HOME/miniconda
-
-# git setting
-git config --global core.editor vi
-git config --global merge.tool vimdiff
-git config --global merge.conflictstyle diff3
-git config --global mergetool.prompt false
-git config --global credential.helper "cache --timeout 604800"
-git config --global core.pager "LESSCHARSET=utf-8 less"
-
-# Python formatter
-conda activate base
-pip install black isort flake8 autopep8
-
-# Update all
-sudo apt upgrade -y && echo "system restarting!" && sudo reboot
+sudo apt update -y && upgrade -y && echo "system restarting!" && sudo reboot
