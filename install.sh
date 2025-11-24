7f7194e425988f79a571a31b19a10f8fb6c30c6a
takahashi <7567050+MasaYan24@users.noreply.github.com>
2025-11-25 05:02:22 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 
-# install shell and basic utilities
-sudo apt install -y zsh build-essential procps file git unzip openssh-server curl cifs-utils keychain
-sudo apt install -y gcc make pkg-config libglvnd-dev  # for NVIDIA driver
-sudo apt install -y tmux
-
-# install developping tools
-sudo apt install -y make g++ direnv ruby libfuse2
-sudo apt install -y imagemagick
-# git clone https://github.com/stefanhaustein/TerminalImageViewer.git /tmp/TIV \
-#     && cd /tmp/TIV/src/main/cpp \
-#     && make \
-#     && sudo make install
-sudo wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -O /usr/bin/nvim
-sudo chmod a+x /usr/bin/nvim
+# install applications
+sudo apt install -y build-essential g++ gcc libglvnd-dev libfuse2 make pkg-config  # 開発ツール  
+sudo apt install -y cifs-utils direnv file procps unzip  # システム管理・ユーティリティ
+sudo apt install -y keychain openssh-server tmux zsh  # シェル・リモート
+sudo apt install -y imagemagick neovim curl git ruby  # エディタ・画像処理・一般ツール・言語
 
 # install homebrew
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
